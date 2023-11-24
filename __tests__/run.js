066c98f5a736f2317a0cbf004e2b334098d84416
Mehdi GHOULAM <medghouly@gmail.com>
2023-11-24 08:54:36 +0100

    create interpretor, can exec program by name

--- __tests__/run.js
+++ __tests__/run.js
@@ -1,43 +1,20 @@
 /**
  * Main test runner.
  */
-import fs from "fs";
-import { Parser } from "../src/Parser.js";
-import { Translator } from "../src/Translator.js";
-
-const program = `
-program Test;
-var a,b;
-begin
-do begin
-  read(a);
-  b := a + b;
-end while (a != 0);
-write(b);
-end
-`;
+import { Interpretor } from "../src/Interpretor.js";
 
 try {
-  const parser = new Parser();
-  const translator = new Translator();
+  const interpretor = new Interpretor();
 
-  const ast = parser.parse(program);
-  console.log(JSON.stringify(ast, null, 2));
-
-  const pcode = translator.translate(ast);
-  console.log(pcode);
-  // Save the pcode to a file.
-
-  // Convert the array into a single string with each instruction on a new line
-  const pcodeString = pcode.join("\n");
-
-  // Save file in root directory/output.pcode
-  const filePath = fs.realpathSync(".") + "/out/output.pcode";
-
-  // Write the pcode string to the file
-  fs.writeFileSync(filePath, pcodeString);
-
-  console.log("Pcode saved to file:", filePath);
+  // get console arguments
+  const args = process.argv.slice(2);
+  if (args.length === 0) {
+    throw new Error("No file specified!");
+  }
+  const filePath = args[0];
+  interpretor.loader(filePath);
+  await interpretor.interpretor();
+  process.exit(0);
 } catch (error) {
   console.error(`${error.message}`);
 }
